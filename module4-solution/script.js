7219cd0ed7c8cfcd23c1faa25bf57c916675cf37
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2024-11-27 12:49:57 +0500

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,7 +1,5 @@
-// STEP 1: Создаем массив имен
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-// STEP 2: Исправляем объекты для вывода сообщений
 (function(window) {
   var HelloSpeaker = {};
   HelloSpeaker.speak = function(name) {
@@ -18,7 +16,6 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
   window.ByeSpeaker = ByeSpeaker;
 })(window);
 
-// STEP 3: Проходим по массиву и вызываем правильную функцию
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
